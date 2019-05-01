7d32f4b6e48354e1f3248171b80e88ab78a53e2c
Lucas Araújo <lucas.compufc@gmail.com>
2019-05-01 12:45:13 -0300

    Melhorando o uso da transação do spring.
    Remove a criação de uma transação para o método de busca.

--- src/main/java/br/com/touchapps/pontoeletronico/repositories/EmpresaRepository.java
+++ src/main/java/br/com/touchapps/pontoeletronico/repositories/EmpresaRepository.java
@@ -1,13 +1,19 @@
 package br.com.touchapps.pontoeletronico.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.touchapps.pontoeletronico.api.entities.Empresa;
 
+/**
+ * Interface responsável em realizar consultas e acessos no banco de dados na tabela de empresa.
+ *
+ * @author Lucas Araújo
+ * @version 1.0
+ */
 public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
 	
-	@Transactional(readOnly = true)
+	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 	Empresa findByCnpj(String cnpj);
-
 }
